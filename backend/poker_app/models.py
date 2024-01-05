59e4aa8727da114141f6beecd9d75ddbf60ec881
reprise77 <semenzemskov03@yandex.ru>
2024-01-05 17:15:56 +1000

    continuing work on the backend, adding game room logic

--- backend/poker_app/models.py
+++ backend/poker_app/models.py
@@ -4,4 +4,6 @@ from django.contrib.auth.models import User
 
 class Room(models.Model):
     name = models.CharField(max_length=255)
-    players = models.ManyToManyField(User, blank=True)
+    max_players = models.PositiveIntegerField(default=4)
+    players = models.ManyToManyField(User, blank=True, related_name='rooms')
+    invitations = models.ManyToManyField(User, blank=True, related_name='room_invitations')
--- backend/poker_app/views.py
+++ backend/poker_app/views.py
@@ -1,8 +1,10 @@
+from rest_framework.decorators import api_view
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Room
-from .serializers import RoomSerializer
+from .serializers import RoomSerializer, UserSerializer
+from django.contrib.auth.models import User
 
 
 class RoomCreateView(generics.CreateAPIView):
@@ -11,7 +13,8 @@ class RoomCreateView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        room = serializer.save()
+        max_players = self.request.data.get('max_players', 2)
+        room = serializer.save(max_players=max_players)
         room.players.add(self.request.user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
@@ -27,11 +30,21 @@ class RoomJoinView(generics.UpdateAPIView):
         return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
 
 
+class RoomLeaveView(generics.UpdateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+    permission_classes = [IsAuthenticated]
+
+    def update(self, request, *args, **kwargs):
+        room = self.get_object()
+        room.players.remove(request.user)
+        return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
+
+
 class RoomDetail(generics.RetrieveDestroyAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
     permission_classes = [IsAuthenticated]
-    http_method_names = ['get', 'delete']
 
     def get(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -43,3 +56,9 @@ class RoomDetail(generics.RetrieveDestroyAPIView):
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
+@api_view(['GET'])
+def get_user_list(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
