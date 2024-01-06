9ef651199eb4cc95750608793afc6ce4e47318c6
boGGruz <bogdangruzkov@mail.ru>
2024-01-06 16:43:44 +1000

    authentification page ready

--- frontend/src/pages/LoginPage.vue
+++ frontend/src/pages/LoginPage.vue
@@ -1,54 +1,86 @@
 <template>
   <q-page class="column justify-center items-center">
-    <q-card :style="{opacity:0.8}" style="min-width: 500px; background-color: black; border-radius: 10px">
-      <q-card-section class="row justify-center text-h2">
-        <span style="color: antiquewhite">Sign Up</span>
-      </q-card-section>
-      <q-card-section>
-        <q-input class="rounded-borders text-white"
-                 style="background-color: darkred; font-size: x-large"
-                 label="Email"
-                 label-color="white"
-                 color="white"
-                 :input-style="{color: 'white'}"
-                 outlined
-                 v-model="email"
-        >
-        </q-input>
-        <q-input class="rounded-borders text-white q-mt-lg"
-                 style="background-color: darkred; font-size: x-large"
-                 label="Username"
-                 label-color="white"
-                 color="white"
-                 :input-style="{color: 'white'}"
-                 outlined
-                 v-model="username"
-        >
-        </q-input>
-        <q-input class="text-white q-mt-lg rounded-borders"
-                 style="background-color: darkred; font-size: x-large"
-                 label="Password"
-                 label-color="white"
-                 color="white"
-                 :input-style="{color: 'white'}"
-                 outlined
-                 v-model="password"
-        >
-        </q-input>
-      </q-card-section>
+    <q-card :style="{opacity:0.8}"
+            style=" max-width: 25%;
+            background-color: black;
+            border-radius: 10px"
+            class='q-px-lg q-pt-lg full-width column justify-center items-center'
+    >
       <q-card-section class="row justify-center">
-        <q-btn label="Submit"
-               style="background-color: darkred; min-width: 150px"
-               text-color="white"
-               @click="SubmitForm"
-        >
-        </q-btn>
+        <span style="color: antiquewhite; font-size: xxx-large">Sign Up</span>
       </q-card-section>
+      <q-form class='fit column' @submit='submitForm'>
+        <q-card-section class='q-pb-none'>
+          <q-input class="rounded-borders text-white"
+                   style="background-color: darkred; font-size: x-large"
+                   label="Email"
+                   label-color="white"
+                   color="white"
+                   :input-style="{color: 'white'}"
+                   outlined
+                   v-model="email"
+          >
+          </q-input>
+          <q-input class="rounded-borders text-white q-mt-lg"
+                   style="background-color: darkred; font-size: x-large"
+                   label="Username"
+                   label-color="white"
+                   color="white"
+                   :input-style="{color: 'white'}"
+                   outlined
+                   v-model="username"
+          >
+          </q-input>
+          <q-input class="text-white q-mt-lg rounded-borders"
+                   style="background-color: darkred; font-size: x-large"
+                   label="Password"
+                   label-color="white"
+                   color="white"
+                   :input-style="{color: 'white'}"
+                   outlined
+                   v-model="password"
+                   :type="isPwd ? 'password' : 'text'"
+          >
+            <template v-slot:append>
+              <q-icon
+                :name="isPwd ? 'visibility_off' : 'visibility'"
+                class='cursor-pointer'
+                color="white"
+                @click='isPwd = !isPwd'
+              />
+            </template>
+          </q-input>
+        </q-card-section>
+        <q-card-section>
+          <q-banner
+            v-if='errors.length > 0'
+            class='relative fit text-white rounded-borders q-mt-sm q-mb-md'
+            style=' background-color: darkred'
+            @submit.prevent='submitForm'
+          >
+            <q-card-section>
+              <span class="row justify-center" style="font-size: medium" v-for="err in errors" :key="err">
+                {{ err }}
+              </span>
+            </q-card-section>
+            <q-btn round flat size='8px'
+                   @click='closeBanner'
+                   class='q-mt-lg q-mr-md absolute-top-right'
+                   text-color='white'
+                   icon='close'
+            />
+          </q-banner>
+          <div class='row full-width justify-center q-pt-sm q-pb-lg q-px-md'>
+            <q-btn class='col-6' style="background-color: darkred; min-height: 50px" text-color="white" label='Sign up'
+                   type='submit'/>
+          </div>
+        </q-card-section>
+      </q-form>
     </q-card>
   </q-page>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import {ref} from 'vue';
 import {api} from 'src/boot/axios';
 import {LocalStorage} from "quasar";
@@ -58,20 +90,29 @@ const email = ref('');
 const password = ref('');
 const username = ref('');
 
-const SubmitForm = () => {
+const isPwd = ref(true);
 
-  api.post('/auth/users/', {
+const errors = ref<string[]>([]);
+
+const submitForm = () => {
+  errors.value = [];
+  const formData = {
     email: email.value,
     username: username.value,
     password: password.value
-  })
+  }
+
+  if (email.value === '' || username.value === '' || password.value === '')
+    return;
+
+  api.post('/auth/users/', formData)
     .then(response => {
       api.post('/auth/jwt/create/', {
         username: username.value,
         password: password.value
       })
-        .then(response =>{
-          const data = response.data;
+        .then(response1 => {
+          const data = response1.data;
 
           const accessToken = data.access;
           const refreshToken = data.refresh;
@@ -83,11 +124,25 @@ const SubmitForm = () => {
 
           api.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
 
-          Router.push({ path: '/rooms'});
+          Router.push({path: '/rooms'});
         })
+    })
+    .catch(error => {
+      if (error.response && error.response.data) {
+        const errorData = error.response.data;
+        for (const key in errorData) {
+          for (let i = 0; i < errorData[key].length; i++) {
+            errors.value.push(errorData[key][i]);
+          }
+        }
+      }
     })
 }
 
+const closeBanner = () => {
+  errors.value = [];
+  return;
+};
 </script>
 
 <style lang="sass" scoped>
