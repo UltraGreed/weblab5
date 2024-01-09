5c0c978fd7298019cf5cbeb09cbc017702231676
boGGruz <bogdangruzkov@mail.ru>
2024-01-09 22:56:37 +1000

    finaly configured branch

--- backend/poker/settings.py
+++ backend/poker/settings.py
@@ -142,11 +142,11 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 
 #smtp
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'pockercock@gmail.com'
-EMAIL_HOST_PASSWORD = 'ckte qhiq etzp qdpc'
-EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'pockercock@gmail.com'
+# EMAIL_HOST_PASSWORD = 'ckte qhiq etzp qdpc'
+# EMAIL_PORT = 587
 
 
 SIMPLE_JWT = {
@@ -172,6 +172,6 @@ DJOSER = {
     'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
     'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': '#/activate/{uid}/{token}',
-    'SEND_ACTIVATION_EMAIL': True,
+    'SEND_ACTIVATION_EMAIL': False,
     'SERIALIZERS': {},
 }
