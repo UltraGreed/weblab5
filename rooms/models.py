bfae132f771453800d852c805038600056e96b84
ultragreed <agent002004@gmail.com>
2024-01-18 10:26:10 +1000

    api get room list endpoint

--- rooms/models.py
+++ rooms/models.py
@@ -7,7 +7,7 @@ class Room(models.Model):
     name = models.CharField(max_length=255)
     is_game_started = models.BooleanField(default=False)
 
-    max_players = models.PositiveIntegerField(default=2)
+    max_players = models.PositiveIntegerField(default=7)
     players = models.ManyToManyField(PokerUser, blank=True, related_name='rooms')
 
     starting_chips = models.IntegerField(default=1000)
